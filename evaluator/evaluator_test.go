93f6b540bc7d8ef28fc15d005aae6d3034323056
xuxiaoyu011 <xuxiaoyu011@163.com>
2024-04-30 22:54:06 +0800

    eval

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,18 @@
+package evaluator
+
+import "testing"
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input  string
+		expect int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+	
+	for _, tt := range tests {
+		
+	}
+
+}
--- repl/repl.go
+++ repl/repl.go
@@ -6,8 +6,21 @@ import (
 	"io"
 
 	"github.com/fengshux/monkey/lexer"
-	"github.com/fengshux/monkey/token"
+	"github.com/fengshux/monkey/parser"
 )
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 const PROMPT = ">>"
 
@@ -15,7 +28,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -23,9 +36,23 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
